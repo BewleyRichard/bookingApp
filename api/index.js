06135fed5cc844cf575a8093de9718d54cbe505e
BewleyRichard <118364533+BewleyRichard@users.noreply.github.com>
2023-04-01 12:22:03 +0100

    edit

--- api/index.js
+++ api/index.js
@@ -16,11 +16,19 @@ const app = express();
 dotenv.config();
 
 
-if (process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*',(req,res)=>  {res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  });
-}
+// if (process.env.NODE_ENV === 'production'){
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+//     app.get('*',(req,res)=>  {res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//   });
+// }
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 
 
